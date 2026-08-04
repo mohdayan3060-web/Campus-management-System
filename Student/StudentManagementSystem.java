c024650221a32308a9d903ee49858e48747f6970
mohdayan0306-web <mohdayan0306@gmail.com>
2026-08-04 16:02:16 +0530

    Building complete CRUD Application |fetures --> Add Student , View Student , Search Student ,Update Student Profile , Delete Student etc...

--- Student/StudentManagementSystem.java
+++ Student/StudentManagementSystem.java
@@ -12,10 +12,10 @@ Student obj= new Student();
         int choice;
         do { 
             System.out.println("Choose your option");
-            System.out.println("1. Add Student ");
-            System.out.println("2. View Student");
-            System.out.println("3. Search Student");
-            System.out.println("4. Update Student Profile  ");
+            System.out.println("1. Add Student ");  //done 
+            System.out.println("2. View Student");  //done 
+            System.out.println("3. Search Student");  //done 
+            System.out.println("4. Update Student Profile  "); 
             System.out.println("5. Delete ");
             System.out.println("6. Exit ");
             choice=scanner.nextInt();
@@ -51,16 +51,11 @@ Student obj= new Student();
         } while (keepruning);
 
 
-    
-       
-
-      
-
     }
 }
 
 class Student{
-     Scanner scanner= new Scanner(System.in);
+     Scanner scanner= new Scanner(System.in); 
      ArrayList<Student> StudentList=new ArrayList<>();
 
 
@@ -87,7 +82,7 @@ class Student{
     }
 
     
-
+// ADD new Student  .................
 
    void AddStudent(){
      System.err.println("Enter Number Of Student You Want To Add ");
@@ -123,28 +118,91 @@ class Student{
    
 
    }
+//Search Student .............................
 
    void SearchStudent(){
-    System.out.println("search student");
-
+   // Search Student through Name  
+  System.out.println("Enter Student Name : ");
+  String name= scanner.nextLine();
+  for (int i=0; i<StudentList.size();i++){
+        if (StudentList.get(i).name.equals(name)){
+                 System.out.println(StudentList.get(i));
+             }
+    
+  }
+ 
    }
-
+//Update Student Profile .........................
    void UpdateStudentProfile(){
-    System.out.println("update profile ");
+     System.out.println("Enter Student Name : ");
+     String name =scanner.nextLine();
+     for (int i=0; i<StudentList.size();i++){
+           if (StudentList.get(i).name.equals(name)){
+        boolean keepruning=true;
+        int choice;
+
+        do {
+            System.out.println("Choose Field you want to Update ");
+            System.out.println("1. Semester");
+            System.out.println("2. Email Address");
+            System.out.println("3. phone number");
+            System.out.println("4. Back to main menu");
+            choice=scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice){
+                case 1:
+                    System.out.println("Enter Your Current Semster");
+                    int semster= scanner.nextByte();
+                    scanner.nextLine();
+                    StudentList.get(i).semster= semster;
+                    System.out.println("Your Updated Semster is :"+StudentList.get(i).semster);
+                    break;
+
+                case 2:
+                    System.out.println("Enter Your New Email :");
+                    String email=scanner.nextLine();
+                    StudentList.get(i).emailId=email;
+                    System.out.println("Your Updated Email-ID : "+StudentList.get(i).emailId);
+                    break;
+                case 3:
+                    System.out.println("Enter Your New Phone Number: ");
+                    String pNumber= scanner.nextLine();
+                    StudentList.get(i).phoneNumber=pNumber;
+                    System.out.println("You Updated Phone Numebr : "+StudentList.get(i).phoneNumber);
+                    break;  
+
+                case 4:
+                    keepruning =false;
+                    return;
+
+                
+
+            }
+
+            
+        } while (true);
+           }
+     }
 
    }
+
+   //Delete particualr Field......
    void Delete(){
-    System.out.println("deleate student ");
+    System.out.println("Enter Student Name You want to Deleate from DataSets :");
+    String name = scanner.nextLine();
+    for(int i=0; i<StudentList.size();i++){
+         if(StudentList.get(i).name.equals(name)){
+            StudentList.remove(i);
+            System.out.println("Field is Deleated .....");
+         }
+    }
 
    }
-
-
-
-
 
 @Override  
 public String toString(){
- return (this.name+ this.studentID+" , " +this.department+" , " +this.semster+" , "+this.emailId+" , "+this.phoneNumber);
+ return ("Name  :"+this.name+" Student ID : "+ this.studentID+" , Department :  " +this.department+" , Semester :  " +this.semster+" , Email ID :  "+this.emailId+" , Phone Number : "+this.phoneNumber);
 
 }
 
